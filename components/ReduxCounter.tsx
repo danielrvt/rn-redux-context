9c3f9231e5085a58160a54eeb8f2c49079b513b6
Daniel Rodriguez del Villar <dani.tri88@gmail.com>
2022-09-13 16:23:52 -0400

    persistence with redux

--- components/ReduxCounter.tsx
+++ components/ReduxCounter.tsx
@@ -1,12 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useLayoutEffect } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
-import { Provider, useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "../store/redux/counter";
+import { useDispatch, useSelector } from "react-redux";
+import { setCount, decrement, increment } from "../store/redux/counter";
 
 function ReduxCounter() {
   const count = useSelector((store: any) => store.counter);
   const dispatch = useDispatch();
   const handleIncrement = () => dispatch(increment());
   const handleDecrement = () => dispatch(decrement());
+
+  useLayoutEffect(() => {
+    AsyncStorage.getItem("COUNTER_REDUX::COUNT")
+      .then((storedCount) => {
+        const savedCount = parseInt(storedCount ?? "0", 10);
+        if (savedCount === 0) {
+          return;
+        }
+        dispatch(setCount(savedCount));
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  useEffect(() => {
+    if (count === 0) return;
+    AsyncStorage.setItem("COUNTER_REDUX::COUNT", `${count}`);
+  }, [count]);
 
   return (
     <View style={styles.container}>
--- store/redux/counter.ts
+++ store/redux/counter.ts
@@ -6,10 +6,12 @@ const counterSlice = createSlice({
   reducers: {
     increment: (state) => state + 1,
     decrement: (state) => state - 1,
+    setCount: (state, action) => action.payload,
   },
 });
 
 export const increment = counterSlice.actions.increment;
 export const decrement = counterSlice.actions.decrement;
+export const setCount = counterSlice.actions.setCount;
 
 export default counterSlice.reducer;
